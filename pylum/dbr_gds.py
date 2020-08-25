884a03246ce54eb20558cf40022e8f292988f286
joaquin <j>
2020-08-25 15:08:51 -0700

    renamed notebooks, dbr_gds accepts function

--- pylum/dbr_gds.py
+++ pylum/dbr_gds.py
@@ -14,9 +14,8 @@ from pylum.config import CONFIG
 from pylum.config import materials
 
 
-@autoname
-def dbr_sweep(deltas, **kwargs):
-    """ simulate DBR unit cell in FDTD
+def dbr_gds_sweep(deltas, **kwargs):
+    """ simulate DBR unit cell in FDTD from a gdsfactory DBR
 
     Args:
         deltas: teeth have [width + delta/2, width - delta/2]
@@ -34,7 +33,7 @@ def dbr_sweep(deltas, **kwargs):
     bw = np.zeros_like(deltas)
 
     for i, delta in tqdm(enumerate(deltas)):
-        d = dbr(delta=delta, **kwargs)
+        d = dbr_gds(delta=delta, **kwargs)
         w0[i] = d["wavelength0"]
         bw[i] = d["bandwidth"]
 
@@ -42,35 +41,43 @@ def dbr_sweep(deltas, **kwargs):
     return dict(session=session, dw=deltas, w0=w0, bw=bw)
 
 
+def unity(x, **kwargs):
+    return x
+
+
 @autoname
-def dbr(
+def dbr_gds(
     session=None,
     run=True,
-    fsp=str(CONFIG["dbr3"]),
+    fsp=str(CONFIG["dbr2"]),
+    gdspath=None,
     material_wg="si",
     material_clad="sio2",
     wg_height=220e-9,
-    sim_time=1e-15 * (2500),
     period=318e-9,
     width=500e-9,
-    width_margin=1e-6,
-    height_margin=1e-6,
     delta=80e-9,
     slab_height=0,
-    mesh_accuracy=2,
+    sim_time=1e-15 * (2500),
+    width_margin=1e-6,
+    height_margin=1e-6,
+    with_slab=False,
+    gdsfunction=unity,
+    cell_name=None,
+    **kwargs,
 ):
-    """ simulate DBR unit cell in FDTD
+    """ simulate DBR unit cell in FDTD from gdsfactory
 
     Args:
         session: lumapi.FDTD session
         run: runs or only draws
         material_wg: 'si' or 'sin'
+        material_clad: 'sio2'
         wg_height: 220e-9
         width: 500e-9
         delta: delta width
         sim_time: depends on grating strength
 
-    FIXME! has some issues with the template, the bandsolve does not work
     """
     l1 = period / 2 * 1e6
     l2 = period / 2 * 1e6
@@ -78,10 +85,15 @@ def dbr(
     w2 = (width + delta / 2) * 1e6
     layer = (1, 0)
 
-    c = pp.c.dbr(w1=w1, w2=w2, l2=l2, l1=l1, n=3)
-    c.x = 0
-    c.flatten()
-    gdspath = pp.write_gds(c)
+    if gdspath is None:
+        c = pp.c.dbr(w1=w1, w2=w2, l2=l2, l1=l1, n=3)
+        c.x = 0
+        c.flatten()
+        gdspath = pp.write_gds(c)
+
+    cell_name = cell_name or c.name
+
+    gdspath = gdsfunction(gdspath, **kwargs)
 
     if material_wg not in materials:
         raise ValueError(f"{material_wg} not in {list(materials.keys())}")
@@ -93,51 +105,61 @@ def dbr(
     s.selectall()
     s.deleteall()
 
-    x_min = -period / 2
-    x_max = period / 2
-    y_max = width + delta / 2 + width_margin
+    # x_min = -period / 2
+    # x_max = period / 2
+    y_max = width / 2 + delta / 2 + width_margin
     y_min = -y_max
     z_min = -height_margin
     z_max = wg_height + height_margin
 
-    x_span = x_max - x_min
+    x_span = period
     y_span = y_max - y_min
     z_span = z_max - z_min
 
     s.load(fsp)
     s.switchtolayout()
 
-    s.addrect(
-        x_min=x_min,
-        x_max=x_max,
-        y_min=y_min,
-        y_max=y_max,
-        z_min=z_min,
-        z_max=z_max,
-        index=1.5,
-        name="SiO2",
-    )
-    s.setnamed("SiO2", "material", materials[material_clad])
+    # s.addrect(
+    #     x_min=x_min,
+    #     x_max=x_max,
+    #     y_min=y_min,
+    #     y_max=y_max,
+    #     z_min=z_min,
+    #     z_max=z_max,
+    #     index=1.5,
+    #     name="clad",
+    # )
+    # s.setnamed("clad", "material", materials[material_clad])
+    s.select("bragg")
+    s.delete()
 
-    s.addfdtd(
-        dimension="3D",
-        x_min=x_min,
-        x_max=x_max,
-        y_min=y_min,
-        y_max=y_max,
-        z_min=z_min,
-        z_max=z_max,
-        mesh_accuracy=mesh_accuracy,
-        use_early_shutoff=True,
-    )
+    s.setnamed("::model", "ax", period)
+    s.setnamed("::model", "width", width)
+    s.setnamed("::model", "delta", delta)
+    s.setnamed("::model", "slab", with_slab)
+    s.setnamed("slab", "z span", slab_height)
 
-    s.gdsimport(str(gdspath), c.name, f"{layer[0]}:{layer[1]}")
+    # s.addfdtd(
+    #     dimension="3D",
+    #     x_min=x_min,
+    #     x_max=x_max,
+    #     y_min=y_min,
+    #     y_max=y_max,
+    #     z_min=z_min,
+    #     z_max=z_max,
+    #     mesh_accuracy=mesh_accuracy,
+    #     use_early_shutoff=True,
+    # )
+    # s.setnamed("FDTD", "simulation time", sim_time)
+
     silicon = f"GDS_LAYER_{layer[0]}:{layer[1]}"
+    s.select(silicon)
+    s.delete()
+    s.gdsimport(str(gdspath), cell_name, f"{layer[0]}:{layer[1]}")
 
     s.setnamed(silicon, "z span", wg_height)
     s.setnamed(silicon, "material", materials[material_wg])
 
-    s.setnamed("FDTD", "simulation time", sim_time)
     s.setnamed("bandstructure", "x", 0)
     s.setnamed("bandstructure", "x span", x_span)
     s.setnamed("bandstructure", "y span", y_span)
@@ -164,4 +186,4 @@ def dbr(
 
 
 if __name__ == "__main__":
-    d = dbr()
+    d = dbr_gds(cache=False)
